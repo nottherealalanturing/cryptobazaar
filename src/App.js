af80cb8f3744ce61445b485dd5db7fc6d5c7c43f
Assad Isah <nottherealalanturing@gmail.com>
2022-06-21 13:06:17 +0100

    feat: add routes, basic navigation and header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-function App() {
-  return <p>empty</p>;
-}
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Layout } from './components';
+import { HomePage, DetailsPage, NotFound } from './pages';
+
+const App = () => (
+  <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<HomePage />} />
+      <Route path="coin" element={<DetailsPage />} />
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  </Routes>
+);
 
 export default App;
