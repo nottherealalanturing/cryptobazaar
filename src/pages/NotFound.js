6ae3b90641cd37d6648791b6588644b8028467f2
Assad Isah <nottherealalanturing@gmail.com>
2022-06-25 15:31:48 +0100

    docs: update READM.md

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const NotFound = () => <div> Page NotFound </div>;
-
+const NotFound = () => (
+  <div>
+    <h1>Oops! you have navigated to an invalid page link, go back</h1>
+    <Link to="/">HOME</Link>
+  </div>
+);
 export default NotFound;
