d52abb4612da846a4fae12dbfde1bfd2a93de7f1
Assad Isah <nottherealalanturing@gmail.com>
2022-06-25 14:44:04 +0100

    test: add snapshot test for home page

---
+++ src/__tests__/home.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import store from '../redux/store';
+import Home from '../pages/Home';
+
+describe('Home page test', () => {
+  it('renders home page', () => {
+    const app = renderer
+      .create(
+        <Provider store={store}>
+          <Home />
+        </Provider>
+      )
+      .toJSON();
+    expect(app).toMatchSnapshot();
+  });
+});
